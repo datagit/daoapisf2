90b3aba8a0dda85621e14a1a8184ebfb2253e1e7
Dat.Dao <datagit@yahoo.com>
2016-04-21 15:15:23 +0700

    add command compare key

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ ApiSf2
 ======
 
 A Symfony project created on November 3, 2015, 10:11 am.
+
+1. authenticate
+2. signature ~ check sum
+3. logger
+4. api short document
+5. test script
+6. using python call Jmeter tool for test API
--- src/Dao/DataSourceBundle/Controller/DefaultController.php
+++ src/Dao/DataSourceBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Dao\DataSourceBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\Yaml\Yaml;
 
 class DefaultController extends Controller
 {
@@ -14,6 +15,9 @@ class DefaultController extends Controller
      */
     public function indexAction($name)
     {
+
+        $a = Yaml::parse(file_get_contents('/var/www/html/dao/daoapisf2/app/config/parameters.yml'));
+        var_dump($a);
         return array('name' => $name);
     }
 }
